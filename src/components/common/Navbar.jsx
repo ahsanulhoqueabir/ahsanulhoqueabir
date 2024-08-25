69b25ede9e1ee4547bcbd3b211352d48266357db
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2024-08-25 18:08:42 +0600

    Added "font-philosopher" to navbar ul className and modified li className.

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,11 +1,11 @@
 const Navbar = ({ page, setPage }) => {
   const navs = ["About", "Resume", "Skills", "Portfolio", "Contact"];
   return (
-    <ul className=" flex flex-wrap justify-center items-center gap-4 md:gap-8">
+    <ul className=" flex flex-wrap justify-center font-philosopher items-center gap-4 md:gap-8">
       {navs?.map((item) => (
         <li
           key={item}
-          className={` font-semibold text-[15px] hover:text-primary  duration-300 ${
+          className={` font-semibold text-[15px] hover:text-primary  duration-300  ${
             page === item ? " text-primary duration-300" : ""
           }`}
         >
